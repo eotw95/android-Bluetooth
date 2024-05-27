d1ae0409c095b59eb493b26b298470527e6689b9
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-05-27 10:48:16 +0900

    Add dialog of permission for scaning bluetooth device

--- app/src/main/java/com/eotw95/samplebluetooth/MainActivity.kt
+++ app/src/main/java/com/eotw95/samplebluetooth/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.eotw95.samplebluetooth
 
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,6 +21,7 @@ import com.eotw95.samplebluetooth.ui.theme.SampleBluetoothTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        showBluetoothDialog()
         setContent {
             SampleBluetoothTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,11 +29,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val bluetoothManager = getSystemService(BluetoothManager::class.java)
-                    val bluetoothAdapter = bluetoothManager.adapter
-                    if (bluetoothAdapter == null) println("Device doesn't support Bluetooth")
+                    Button(onClick = { showBluetoothDialog() }) {
+                        Text(text = "show bluetooth dialog")
+                    }
                 }
             }
+        }
+    }
+
+    private fun showBluetoothDialog() {
+        // ① get BluetoothAdapter
+        val bluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter = bluetoothManager.adapter
+        if (bluetoothAdapter == null) println("Device doesn't support Bluetooth")
+
+        // ② enable bluetooth of device
+        if (!bluetoothAdapter.isEnabled) {
+            // Bluetoothで付近のデバイスを検出することの許可をユーザーに求めるダイアログの表示
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    // 許可を選択したら通るルート
+                } else {
+                    // 拒否を選択したら通るルート
+                }
+            }.launch(android.Manifest.permission.BLUETOOTH_CONNECT)
         }
     }
 }
