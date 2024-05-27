5cc95d3a498cfb863790dca4ead78101b62bf188
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-05-27 11:41:02 +0900

    Add another Bluetooth permission for defferent SDK version

--- app/src/main/java/com/eotw95/samplebluetooth/MainActivity.kt
+++ app/src/main/java/com/eotw95/samplebluetooth/MainActivity.kt
@@ -3,6 +3,8 @@ package com.eotw95.samplebluetooth
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Intent
+import android.os.Build
+import android.os.Build.VERSION
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -42,7 +44,6 @@ class MainActivity : ComponentActivity() {
         val bluetoothAdapter = bluetoothManager.adapter
         if (bluetoothAdapter == null) println("Device doesn't support Bluetooth")
 
-        // ② bluetoothAdapterがnullでなければ、Bluetoothで付近のデバイスを検出することの許可をユーザーに求めるダイアログの表示
         requestBtPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
                 // 許可を選択したら通るルート
@@ -66,7 +67,10 @@ class MainActivity : ComponentActivity() {
             if (it.resultCode == RESULT_OK) scanDevice() else println("result is ng")
         }
 
-        requestBtPermissionLauncher.launch(android.Manifest.permission.BLUETOOTH_CONNECT)
+        if (VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // ② bluetoothAdapterがnullでなければ、Bluetoothで付近のデバイスを検出することの許可をユーザーに求めるダイアログの表示
+            requestBtPermissionLauncher.launch(android.Manifest.permission.BLUETOOTH_CONNECT)
+        } else requestBtPermissionLauncher.launch(android.Manifest.permission.BLUETOOTH)
     }
 
     private fun scanDevice() {}
