9f53bc22ce9a8ec4f1d275fa4946b4d517870d48
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-05-27 18:31:51 +0900

    Add startScan ble device

--- app/src/main/java/com/eotw95/samplebluetooth/MainActivity.kt
+++ app/src/main/java/com/eotw95/samplebluetooth/MainActivity.kt
@@ -1,33 +1,36 @@
 package com.eotw95.samplebluetooth
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Build.VERSION
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.registerForActivityResult
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.eotw95.samplebluetooth.ui.theme.SampleBluetoothTheme
 
 class MainActivity : ComponentActivity() {
     private lateinit var requestBtPermissionLauncher: ActivityResultLauncher<String>
     private lateinit var enableBtLauncher: ActivityResultLauncher<Intent>
+    private lateinit var requestBtScanPermission: ActivityResultLauncher<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        showBluetoothDialog()
+        setupBle()
         setContent {
             SampleBluetoothTheme {
                 // A surface container using the 'background' color from the theme
@@ -39,17 +42,17 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun showBluetoothDialog() {
-        // ① get BluetoothAdapter
-        val bluetoothManager = getSystemService(BluetoothManager::class.java)
-        val bluetoothAdapter = bluetoothManager.adapter
-
-        if (bluetoothAdapter == null) println("Device doesn't support Bluetooth")
-
+    private fun setupBle() {
         // BLEがサポートされているデバイスかどうか確認
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             println("BLE is not supported")
         } else println("BLE is supported")
+
+        // BluetoothAdapterを取得
+        val bluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter = bluetoothManager.adapter
+
+        if (bluetoothAdapter == null) println("Device doesn't support Bluetooth")
 
         requestBtPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
@@ -73,12 +76,64 @@ class MainActivity : ComponentActivity() {
         enableBtLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == RESULT_OK) scanDevice() else println("result is ng")
         }
+        requestBtScanPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) println("not permitted BLE scan") else println("not permitted BLE scan")
+        }
 
         if (VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            // ② bluetoothAdapterがnullでなければ、Bluetoothで付近のデバイスを検出することの許可をユーザーに求めるダイアログの表示
-            requestBtPermissionLauncher.launch(android.Manifest.permission.BLUETOOTH_CONNECT)
-        } else requestBtPermissionLauncher.launch(android.Manifest.permission.BLUETOOTH)
+            // bluetoothAdapterがnullでなければ、Bluetoothで付近のデバイスを検出することの許可をユーザーに求めるダイアログの表示
+            requestBtPermissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT)
+        } else requestBtPermissionLauncher.launch(Manifest.permission.BLUETOOTH)
+
+        // BluetoothScannerを取得
+        val bluetoothScanner = bluetoothAdapter.bluetoothLeScanner
+        val bleScanCallback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult?) {
+                super.onScanResult(callbackType, result)
+                println("onScanResult result=$result")
+            }
+        }
+
+        // Check if permission of BLE Scan is enabled
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED
+        ) {
+            println("BLUETOOTH_SCAN permitted")
+        } else {
+            println("BLUETOOTH_SCAN not permitted")
+            requestBtScanPermission.launch(Manifest.permission.BLUETOOTH_SCAN)
+        }
+
+        // startScan ble device
+        val handler = Handler()
+        var scanning = false
+        val SCAN_PERIOD: Long = 10000
+        if (!scanning) {
+            handler.postDelayed(
+                {
+                    scanning =  false
+                    bluetoothScanner.stopScan(bleScanCallback)
+                },
+                SCAN_PERIOD
+            )
+            scanning = true
+            bluetoothScanner.startScan(bleScanCallback)
+        } else {
+            scanning = false
+            bluetoothScanner.stopScan(bleScanCallback)
+        }
     }
 
     private fun scanDevice() {}
+
+    private fun buildScanFilters(): List<ScanFilter> {
+        val scanFilters = mutableListOf<ScanFilter>()
+        val scanFilter = ScanFilter.Builder().setServiceUuid(null).build()
+        scanFilters.add(scanFilter)
+        return scanFilters
+    }
+    private fun buildScanSettings(): ScanSettings {
+        return ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_POWER).build()
+    }
 }
