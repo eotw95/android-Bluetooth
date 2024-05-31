f72e3aeffb33642b855eae283d41f95c17f0e1da
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-05-31 14:38:12 +0900

    Add method for scan ble device

--- app/src/main/java/com/eotw95/samplebluetooth/MainActivity.kt
+++ app/src/main/java/com/eotw95/samplebluetooth/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.eotw95.samplebluetooth
 
+import android.Manifest
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import com.eotw95.samplebluetooth.ui.theme.SampleBluetoothTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +35,42 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {}
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(onClick = { scanBleDevice() }) {
+                            Text(text = "Scan BLE")
+                        }
+                    }
+                }
             }
         }
+    }
+
+    private fun scanBleDevice() {
+        val btManager = getSystemService(BluetoothManager::class.java)
+        val btScanner = btManager.adapter.bluetoothLeScanner
+
+        if (btManager.adapter != null && btManager.adapter.isEnabled) Log.d("scanBleDevice", "ble adapter is enable")
+        else Log.d("scanBleDevice", "ble adapter is not enable")
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_SCAN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("scanBleDevice", "BLUETOOTH_SCAN is not granted")
+        }
+
+        val bleScanCallback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult?) {
+                super.onScanResult(callbackType, result)
+                Log.d("BleScanCallback", "result:$result")
+            }
+        }
+        btScanner.startScan(bleScanCallback)
     }
 }
